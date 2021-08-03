df26ad329cd6ae73a1087ef1f40c666510a786d4
ShalluMehta <88322226+ShalluMehta@users.noreply.github.com>
2021-08-03 09:00:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+function setup() 
+{
+  createCanvas(1000,1000);
+  background("blue")
+
+
+  
+
+}
+
+
+function draw() {
+if(keyDown(DOWN_ARROW)) {
+background("red")
+}
+if(keyDown(LEFT_ARROW)) {
+background("yellow")
+}
+if(keyDown(UP_ARROW)) {
+background("green")
+}
+if(keyDown(RIGHT_ARROW)) {
+background("magenta") }
+}
+  
+   
